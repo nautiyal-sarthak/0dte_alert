9053654003fcdbc0c93f891ed6d314461979cd4b
Sarthak <nautiyal.sarthak@gmail.com>
2026-03-02 19:48:08 -0500

    dev

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ def should_consider_trade(features: dict) -> bool:
 def main():
 
     ##"2026-02-12" -- big down day
-    date_in = None #"2026-02-26" #"2026-02-23" #"2026-02-23" #"2026-01-29" #"2026-01-30" # live
-    time_in = None #"12:00:00" #"09:30:00" #"09:30:00" #"10:30:00" #"10:30:00"
+    date_in = "2026-03-02" #"2026-02-26" #"2026-02-23" #"2026-02-23" #"2026-01-29" #"2026-01-30" # live
+    time_in = "09:30:00" #"12:00:00" #"09:30:00" #"09:30:00" #"10:30:00" #"10:30:00"
     config = load_config()
 
     state = load_last_alert_state()
@@ -219,7 +219,7 @@ def main():
         print("-" * 50)
 
         # exit the loop if we latest.name.strftime('%Y-%m-%d %H:%M:%S') is equal to  current_day_end
-        if latest.name >= current_day_end:
+        if latest.name >= current_day_end.tz_localize(latest.name.tzinfo):
             print(f"Reached end of day ({current_day_end}), exiting.")
             break
 
